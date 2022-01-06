62a903d45ae0693bccc3743cea581653e00d2775
HinashiSenpai <96591580+HinashiSenpai@users.noreply.github.com>
2022-01-06 14:04:10 +0530

    Update Task-2.py

--- Day 2/Task-2.py
+++ Day 2/Task-2.py
@@ -3,4 +3,4 @@ Term_2= int(input("Enter Total Marks Of Term 2: "))
 Term_3= int(input("Enter Total Marks Of Term 3: "))
 Total= Term_1+Term_2+Term_3
 Per=Total/5
-print(" Your Total Percentage is:",Per,"%")
+print(" The Total Percentage Of The Semister:",Per,"%")
